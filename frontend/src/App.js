869bd3036a2b10d9c62e205c33bbfd5ab13b78ea
TimXia7 <timxia6208@gmail.com>
2026-04-25 20:38:05 -0400

    NEW FRONTEND: added task list template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "main">
+        <div className = "mainContainer">
+          <header>
+            <h1> </h1> {/* <h1>HomeBase</h1> */}
+          </header>
+
+          <main>
+            <section>
+              <h2>Tasks</h2>
+
+              <div className="inputRow">
+                <input style={{outline: "1px solid #4f4d61"}} placeholder="Enter a task..." />
+                <button style={{outline: "1px solid #4f4d61"}}>Add</button>
+              </div>
+
+              <div className="taskContainer">
+                <div className="taskItem">
+                  <span>Example Task</span>
+
+                  <div className="taskButtons">
+                    <button style={{outline: "1px solid #4f4d61"}} className="taskBtn"></button>
+                    <button style={{outline: "1px solid #4f4d61"}} className="taskBtn"></button>
+                  </div>
+                </div>
+              </div>
+            </section>
+
+            {/* <section>
+              <h2>Map</h2>
+              <div style={{ height: "300px", background: "#eee" }}>
+                Map placeholder
+              </div>
+            </section> */}
+          </main>
+        </div>
     </div>
   );
 }
