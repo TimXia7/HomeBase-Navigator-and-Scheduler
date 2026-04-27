7dfe667ff79def2f3a0dae8a9d74d87f53893c77
TimXia7 <timxia6208@gmail.com>
2026-04-27 19:48:37 -0400

    NEW: leaflet test for map data

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './styles/global.css';
 import App from './App';
+import "leaflet/dist/leaflet.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
