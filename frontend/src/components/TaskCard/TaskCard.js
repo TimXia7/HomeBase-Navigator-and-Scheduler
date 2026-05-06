f528c30614c201b41692d0573f6ee21c9216a9e0
TimXia7 <timxia6208@gmail.com>
2026-05-06 04:27:38 -0400

    DOCS: TaskCard docs

--- frontend/src/components/TaskCard/TaskCard.js
+++ frontend/src/components/TaskCard/TaskCard.js
@@ -1,15 +1,21 @@
 import { useDraggable } from "@dnd-kit/core";
 
 function TaskCard({ task }) {
+
+  // Make this task card draggable using its unique task id
   const { attributes, listeners, setNodeRef, isDragging } = useDraggable({
     id: task.id,
   });
 
   return (
     <div
+      // Tell dnd-kit which DOM element should be draggable
       ref={setNodeRef}
+
+      // Add a dragging class while the task is being dragged
       className={`taskCard ${isDragging ? "taskCardDragging" : ""}`}
 
+      // Add dnd-kit drag event handlers and accessibility attributes
       {...listeners}
       {...attributes}
     >
